94082d8c2e7dc2d24d7639d8f74468a031601f0f
Huferr <hugoferreirait@gmail.com>
2021-07-05 23:51:10 -0300

    Finishing Tab Component

--- src/components/Tab/index.js
+++ src/components/Tab/index.js
@@ -1,21 +1,51 @@
 import React, { useState } from "react";
-import './styles.css'
+import "./styles.css";
 
 const Tab = () => {
+  const [ToggleState, setToggleState] = useState(1);
 
-    const [ToggleState, setToggleState] = useState(1)
-    
+  const ToggleTab = (index) => {
+    setToggleState(index);
+  };
+
   return (
     <div className="container">
       <ul className="tab-list">
-        <li className="tabs active-tabs">Tab 1</li>
-        <li className="tabs">Tab 2</li>
-        <li className="tabs">Tab 3</li>
+        <li
+          className={ToggleState === 1 ? "tabs active-tabs" : "tabs"}
+          onClick={() => ToggleTab(1)}
+        >
+          Tab 1
+        </li>
+        <li
+          className={ToggleState === 2 ? "tabs active-tabs" : "tabs"}
+          onClick={() => ToggleTab(2)}
+        >
+          Tab 2
+        </li>
+        <li
+          className={ToggleState === 3 ? "tabs active-tabs" : "tabs"}
+          onClick={() => ToggleTab(3)}
+        >
+          Tab 3
+        </li>
       </ul>
       <div className="content-container">
-        <div className="content active-content"><h2>Lorem</h2></div>
-        <div className="content"><h2>Ipsum</h2></div>
-        <div className="content"><h2>Dolor</h2></div>
+        <div
+          className={ToggleState === 1 ? "content active-content" : "content"}
+        >
+          <h2>Lorem</h2>
+        </div>
+        <div
+          className={ToggleState === 2 ? "content active-content" : "content"}
+        >
+          <h2>Ipsum</h2>
+        </div>
+        <div
+          className={ToggleState === 3 ? "content active-content" : "content"}
+        >
+          <h2>Dolor</h2>
+        </div>
       </div>
     </div>
   );
